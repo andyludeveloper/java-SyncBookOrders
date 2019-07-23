57537bc184a4dc11a43156351e2d061fc5cb9e46
Joey Chen <jay.hatelove@gmail.com>
2019-07-23 18:03:42 +0800

    lab

--- src/main/java/com/odde/isolated/OrderService.java
+++ src/main/java/com/odde/isolated/OrderService.java
@@ -10,32 +10,24 @@ public class OrderService {
 
     private String filePath = "C:\\temp\\testOrders.csv";
 
-    public void syncBookOrders()
-    {
-        List<Order> orders = this.getOrders();
+    public void syncBookOrders() {
+        List<Order> orders = this.GetOrders();
 
         // only get orders of book
         List<Order> ordersOfBook = orders.stream().filter(x -> x.getType().equals("Book")).collect(toList());
 
-        BookDao bookDao = getBookDao();
-        for (Order order : ordersOfBook)
-        {
+        BookDao bookDao = new BookDao();
+        for (Order order : ordersOfBook) {
             bookDao.insert(order);
         }
     }
 
-    protected BookDao getBookDao() {
-        return new BookDao();
-    }
-
-    protected List<Order> getOrders()
-    {
+    private List<Order> GetOrders() {
         // parse csv file to get orders
         List<Order> result = new ArrayList<>();
 
         // directly depend on File I/O
-        try (Scanner scanner = new Scanner(filePath))
-        {
+        try (Scanner scanner = new Scanner(filePath)) {
             int rowCount = 0;
 
             while (scanner.hasNextLine()) {
@@ -56,9 +48,8 @@ public class OrderService {
         return result;
     }
 
-    private Order Mapping(String[] line)
-    {
-        Order result = new Order(){{
+    private Order Mapping(String[] line) {
+        Order result = new Order() {{
             setProductName(line[0]);
             setType(line[1]);
             setPrice(Integer.parseInt(line[2]));
--- src/test/java/com/odde/isolated/OrderServiceTest.java
+++ src/test/java/com/odde/isolated/OrderServiceTest.java
@@ -2,45 +2,13 @@ package com.odde.isolated;
 
 import org.junit.Test;
 
-import java.util.function.Consumer;
-
-import static java.util.Arrays.asList;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
-
 public class OrderServiceTest {
-
-    private BookDao mockBookDao = mock(BookDao.class);
-
-    OrderService target = spy(OrderService.class);
 
     @Test
     public void syncbookorders_3_orders_only_2_book_order() {
         // hard to isolate dependency to unit test
-        givenOrders(new Order() {{
-            setType("Book");
-        }}, new Order() {{
-            setType("CD");
-        }}, new Order() {{
-            setType("Book");
-        }});
-        when(target.getBookDao()).thenReturn(mockBookDao);
 
-        target.syncBookOrders();
-
-        verify(mockBookDao, times(2)).insert(
-                should(order -> assertThat(order.getType()).isEqualTo("Book")));
+        //OrderService target = new OrderService();
+        //target.SyncBookOrders();
     }
-
-    private void givenOrders(Order... orders) {
-        when(target.getOrders()).thenReturn(asList(orders));
-    }
-
-    public static <T> T should(Consumer<T> assertion) {
-        return argThat(argument -> {
-            assertion.accept(argument);
-            return true;
-        });
-    }
-
 }
